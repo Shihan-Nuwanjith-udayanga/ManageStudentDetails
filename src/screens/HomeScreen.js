0a066baa092e4695387192bb842846c74771f7d3
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 19:53:28 +0530

    create HomeScreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default HomeScreen;
