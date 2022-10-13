a274314bd26b2e183885ee8bc81187bf3dfc3d04
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 22:14:03 +0530

    create AddStudentDetailsScreen

---
+++ src/screens/AddStudentDetailsScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+function AddStudentDetailsScreen() {
+  return (
+    <View>
+      <Text>AddStudentDetailsScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default AddStudentDetailsScreen;
