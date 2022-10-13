cc3e8becd0b41ae7fd7c1e865b70327662b2c08b
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-14 02:28:32 +0530

    create navigations

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/screens/LoginScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const App = () => {
+import LoginScreen from './src/screens/LoginScreen';
+import AppTextInput from './src/components/AppTextInput';
+import AppButton from './src/components/AppButton';
+import SignupScreen from './src/screens/SignupScreen';
+import AuthScreen from './src/screens/AuthScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import AddStudentDetailsScreen from './src/screens/AddStudentDetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen options={{headerShown: false}} name="LoginScreen" component={LoginScreen}/>
+        <Stack.Screen options={{headerShown: false}} name="AppTextInput" component={AppTextInput}/>
+        <Stack.Screen options={{headerShown: false}} name="AppButton" component={AppButton}/>
+        <Stack.Screen options={{headerShown: false}} name="SignupScreen" component={SignupScreen}/>
+        <Stack.Screen options={{headerShown: false}} name="AuthScreen" component={AuthScreen}/>
+        <Stack.Screen options={{headerShown: false}} name="HomeScreen" component={HomeScreen}/>
+        <Stack.Screen options={{headerShown: false}} name="AddStudentDetailsScreen" component={AddStudentDetailsScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
