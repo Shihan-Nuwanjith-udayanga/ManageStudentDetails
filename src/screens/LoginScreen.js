3d15ba6231aeee3087c48b493ee568cac6ff4bba
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 08:39:30 +0530

    create Login Screen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const LoginScreen = () => {
+  return (
+    <View></View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default LoginScreen;
