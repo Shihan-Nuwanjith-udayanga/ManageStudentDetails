f1f2466faff7341152d429f7f0f8f6e62d5430b7
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 11:49:09 +0530

    create a AppTextInput View

--- src/components/AppTextInput.js
+++ src/components/AppTextInput.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {TextInput, StyleSheet, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const AppTextInput = () => {
+import colors from '../config/Colors.js';
+
+function AppTextInput({icon, ...otherProps}) {
   return (
-    <div>AppTextInput</div>
-  )
+    <View style={styles.container}>
+      {icon && (
+        <MaterialCommunityIcons name={icon} size={25}
+          color={colors.medium}
+          style={styles.icon}
+        />
+      )}
+      <TextInput
+        placeholderTextColor={colors.medium}
+        style={styles.textInput}
+        {...otherProps}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 15,
+    flexDirection: 'row',
+    width: '95%',
+    height: 60,
+    // padding: 15,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: '#000'
+  },
+
+  icon: {
+    padding: 15,
+  },
+
+  textInput: {
+    fontSize: 18,
+  },
 });
 
 export default AppTextInput;
