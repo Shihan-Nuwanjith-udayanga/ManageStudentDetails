4976d594c53eb0c27f484556ff7d5e79ebaf88e9
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 23:39:25 +0530

    add label

--- src/components/AppTextInput.js
+++ src/components/AppTextInput.js
@@ -1,38 +1,60 @@
 import React from 'react';
-import {TextInput, StyleSheet, View} from 'react-native';
+import {TextInput, StyleSheet, View, Text} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import colors from '../config/Colors.js';
 
-function AppTextInput({icon, ...otherProps}) {
+function AppTextInput({label, icon, ...otherProps}) {
   return (
-    <View style={styles.container}>
-      {icon && (
-        <MaterialCommunityIcons name={icon} size={25}
-          color={colors.medium}
-          style={styles.icon}
+    <View style={styles.mainView}>
+      <View style={styles.labelView}>
+        <Text>{label}</Text>
+      </View>
+      <View style={styles.container}>
+        {icon && (
+          <MaterialCommunityIcons
+            name={icon}
+            size={25}
+            color={colors.medium}
+            style={styles.icon}
+          />
+        )}
+        <TextInput
+          placeholderTextColor={colors.medium}
+          style={styles.textInput}
+          {...otherProps}
         />
-      )}
-      <TextInput
-        placeholderTextColor={colors.medium}
-        style={styles.textInput}
-        {...otherProps}
-      />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  mainView: {
+    width: '100%',
+    justifyContent: 'center',
+    // alignItems: 'center',
+    marginLeft: 15,
+  },
+
+  labelView: {
+    marginBottom: 5,
+    position: 'relative',
+    color: colors.medium,
+    marginLeft: 10,
+  },
+
   container: {
     backgroundColor: colors.light,
-    borderRadius: 15,
+    borderRadius: 10,
     flexDirection: 'row',
     width: '95%',
-    height: 60,
+    // height: 60,
     // padding: 15,
-    marginVertical: 10,
+    // marginVertical: 10,
     // borderWidth: 1,
-    borderColor: colors.medium
+    marginBottom: 20,
+    borderColor: colors.medium,
   },
 
   icon: {
