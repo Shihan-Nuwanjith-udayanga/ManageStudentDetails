368d75699612f9353c8aa133b833bc55ad8b810a
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 09:27:52 +0530

    create AppTextInput file

---
+++ src/components/AppTextInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AppTextInput = () => {
+  return (
+    <div>AppTextInput</div>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export default AppTextInput;
