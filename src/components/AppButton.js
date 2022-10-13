c7add885458989912ad83a8b6eab7f13344974c8
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 13:31:32 +0530

    add onPress Statement

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -3,9 +3,11 @@ import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 import colors from '../config/Colors.js';
 
-function AppButton({title, onPress, color="button"}) {
+function AppButton({title, onPress, color = 'button'}) {
   return (
-    <TouchableOpacity style={[styles.button, {backgroundColor: colors[color]}]}>
+    <TouchableOpacity
+      style={[styles.button, {backgroundColor: colors[color]}]}
+      onPress={onPress}>
       <Text style={styles.text}>hello</Text>
     </TouchableOpacity>
   );
