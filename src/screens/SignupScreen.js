16e8f769c68a2599bc98b0ad2a59420bca6aad63
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 15:50:29 +0530

    create signupScreen

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+unction SignupScreen() {
+  return (
+    <View>
+      <Text>SignupScreen</Text>
+    </View>
+  )
+}
+
+export default SignupScreen;
