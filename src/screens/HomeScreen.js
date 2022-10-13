6140a47e9ca5e98cc45470f01a0e57b5fa87b333
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 22:14:39 +0530

    create HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,70 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useNavigation} from '@react-navigation/core';
+
+import colors from '../config/Colors.js';
+import AppButton from '../components/AppButton.js';
+import AddStudentDetailsScreen from './AddStudentDetailsScreen';
 
 function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
+          <MaterialCommunityIcons
+            name="keyboard-backspace"
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+          <MaterialCommunityIcons name="home" style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.button}>
+        <AppButton
+          title="Add New Student"
+          onPress={() => navigation.navigate('AddStudentDetailsScreen')}
+        />
+      </View>
+      <ScrollView style={styles.scrollView}></ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  header: {
+    flexDirection: 'row',
+    padding: 15,
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+
+  icon: {
+    fontSize: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: colors.medium,
+  },
+
+  button: {
+    // flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  scrollView: {
+    backgroundColor: colors.white,
+  },
 });
 
 export default HomeScreen;
