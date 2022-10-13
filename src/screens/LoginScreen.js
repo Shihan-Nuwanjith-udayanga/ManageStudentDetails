2941e396ecf1bc060f95e4ce599cfa7aa7bc538b
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-13 12:08:17 +0530

    create email and password field

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
+// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const LoginScreen = () => {
+import AppTextInput from './../components/AppTextInput.js';
+import colors from '../config/Colors.js';
+
+function LoginScreen() {
   return (
-    <View></View>
-  )
+    <View style={styles.container}>
+      <AppTextInput placeholder="Username" icon="email" name="Username" />
+      <AppTextInput
+        placeholder="Password"
+        icon="form-textbox-password"
+        name="Password"
+        secureTextEntry= {true}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.white,
+  },
 });
 
 export default LoginScreen;
