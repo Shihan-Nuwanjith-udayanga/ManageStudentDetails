9d24d298f68c19a514b8c319aaffe65d32fe2136
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2022-10-14 15:17:31 +0530

    create asyncStorage

--- src/screens/AddStudentDetailsScreen.js
+++ src/screens/AddStudentDetailsScreen.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   ScrollView,
+  Alert,
 } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {useNavigation} from '@react-navigation/core';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import colors from '../config/Colors.js';
 import AppButton from '../components/AppButton.js';
@@ -15,6 +17,57 @@ import AppTextInput from '../components/AppTextInput.js';
 
 function AddStudentDetailsScreen() {
   const navigation = useNavigation();
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [parentName, setParentName] = useState('');
+  const [parentContactNumber, setParentContactNumber] = useState('');
+  const [parentEmail, setParentEmail] = useState('');
+
+  useEffect(() => {
+    setData();
+  },[])
+
+
+  const getData = () => {
+    try {
+      AsyncStorage.getItem('StudentData').then(value => {
+        if (value != null) {
+          navigation.navigate('HomeScreen');
+          Alert.alert('Student Details Add In Successfully...!');
+        }
+      });
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  const setData = async () => {
+    if(firstName.length == 0 || lastName.length == 0 || contactNumber.length == 0 || email.length == 0 ||
+      parentName.length == 0 || parentContactNumber.length == 0 || parentEmail.length == 0){
+        Alert.alert("Warning!", "Please Input Your Data!");
+      }else{
+        try {
+          var details = {
+            FirstName: firstName,
+            LastName: lastName,
+            ContactNumber: contactNumber,
+            Email: email,
+            ParentName: parentName,
+            ParentContactNumber: parentContactNumber,
+            ParentEmail: parentEmail,
+          };
+          await AsyncStorage.setItem('StudentData', JSON.stringify(details));
+          Alert.alert("Student Data Add Successfully...!");
+          console.log(details);
+          navigation.navigate('HomeScreen');
+        } catch (error) {
+          console.warn(error);
+        }
+      }
+  }
 
   return (
     <View style={styles.container}>
@@ -37,6 +90,7 @@ function AddStudentDetailsScreen() {
             name="firstName"
             inputMode="text"
             label="First Name"
+            onChangeText={value => setFirstName(value)}
             style={styles.input}
           />
           <AppTextInput
@@ -44,6 +98,7 @@ function AddStudentDetailsScreen() {
             name="lastName"
             inputMode="text"
             label="Last Name"
+            onChangeText={value => setLastName(value)}
             style={styles.input}
           />
           <AppTextInput
@@ -52,6 +107,7 @@ function AddStudentDetailsScreen() {
             inputMode="tel"
             keyboardType="number-pad"
             label="Contact Number"
+            onChangeText={value => setContactNumber(value)}
             style={styles.input}
           />
           <AppTextInput
@@ -60,6 +116,7 @@ function AddStudentDetailsScreen() {
             inputMode="email"
             keyboardType="email-address"
             label="Email"
+            onChangeText={value => setEmail(value)}
             style={styles.input}
           />
           <AppTextInput
@@ -67,6 +124,7 @@ function AddStudentDetailsScreen() {
             name="parentName"
             inputMode="text"
             label="Parent Name"
+            onChangeText={value => setParentName(value)}
             style={styles.input}
           />
           <AppTextInput
@@ -75,16 +133,22 @@ function AddStudentDetailsScreen() {
             inputMode="tel"
             keyboardType="number-pad"
             label="Parent Contact Number"
+            onChangeText={value => setParentContactNumber(value)}
             style={styles.input}
           />
           <AppTextInput
             placeholder="Parent Email"
-            name="ParentEmail"
+            name="parentEmail"
             inputMode="email"
             keyboardType="email-address"
             label="Parent Email"
+            onChangeText={value => setParentEmail(value)}
             style={styles.input}
           />
+          {/* {title.trim() || desc.trim() ? ( */}
+          <AppButton title="Submit" onPress={setData} />
+          {/* ) : null} */}
+          <View style={{margin: 50}}></View>
         </View>
       </ScrollView>
     </View>
@@ -128,14 +192,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     // backgroundColor: 'gray',
-    marginTop: 10
+    marginTop: 10,
   },
 
   input: {
     fontSize: 16,
-    marginLeft: 10
+    marginLeft: 10,
   },
-
 });
 
 export default AddStudentDetailsScreen;
